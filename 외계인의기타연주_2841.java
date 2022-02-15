d2841a17348f6a3080c8d440b7a7ba67668efd6e
Cho Jeongmin <97157930+Cho-Jeongmin@users.noreply.github.com>
2022-02-16 02:01:45 +0900

    Add files via upload

---
+++ 외계인의기타연주_2841.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class 외계인의기타연주_2841 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int p = Integer.parseInt(st.nextToken());
+		
+		Stack<Integer> stack[] = new Stack[n];
+		for(int i=0; i<n; i++) {
+			stack[i] = new Stack<Integer>();
+		}
+		
+		int cnt = 0;
+		
+		while(n-->0) {
+			st = new StringTokenizer(br.readLine());
+			
+			int line = Integer.parseInt(st.nextToken());
+			int fret = Integer.parseInt(st.nextToken());
+			
+			while(!stack[line].isEmpty() && stack[line].peek() > fret) {
+				stack[line].pop();
+				cnt++;
+			}
+			
+			if(stack[line].isEmpty() || stack[line].peek() < fret) {
+				stack[line].push(fret);
+				cnt++;
+			}
+			
+		}
+		
+		System.out.println(cnt);
+		
+	}
+}
